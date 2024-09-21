613edfb8e48a69e302b54e00d69cf5a897794827
Arden Rasmussen <ardenisthebest@gmail.com>
2024-09-20 18:20:27 -0700

    feat(util): precomputed table of 1000 primes

---
+++ include/specula/util/primes.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file primes.hpp
+ * @brief A table of 1000 precomputed prime numbers.
+ */
+
+#ifndef INCLUDE_UTIL_PRIMES_HPP_
+#define INCLUDE_UTIL_PRIMES_HPP_
+
+#include <cstddef>
+
+#include <specula/specula.hpp>
+
+namespace specula {
+  /**
+   * @brief The size of the precomputed prime table.
+   */
+  static constexpr std::size_t PRIME_TABLE_SIZE = 10000;
+
+  /**
+   * @brief A table of precomputed prime numbers.
+   *
+   * This is used for determining bases to use for the radical inverse base low-discrepancy points.
+   * And it is precomputed to avoid the overhead of computing prime numbers at runtime.
+   */
+  extern SPECULA_CONST int PRIMES[PRIME_TABLE_SIZE];
+} // namespace specula
+
+#endif // INCLUDE_UTIL_PRIMES_HPP_
