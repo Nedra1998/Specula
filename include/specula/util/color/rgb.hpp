6011940721a438fb7834e45e51d5e7d9b694c03a
Arden Rasmussen <ardenisthebest@gmail.com>
2025-07-15 10:25:26 -0700

    docs: add docstring for RGB and XYZ color classes

--- include/specula/util/color/rgb.hpp
+++ include/specula/util/color/rgb.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file rgb.hpp
+ * @brief RGB color class definition and operations
+ *
+ * This file defines the `Rgb` class, which represents a color in the RGB color space.
+ * It includes various operations such as addition, subtraction, multiplication, division,
+ * clamping, and interpolation.
+ */
+
 #ifndef INCLUDE_COLOR_RGB_HPP_
 #define INCLUDE_COLOR_RGB_HPP_
 
@@ -6,11 +15,27 @@
 
 namespace specula {
 
+  /**
+   * @class Rgb
+   * @brief A class representing a color in the RGB color space.
+   *
+   * The `Rgb` class encapsulates a color defined by its red, green, and blue components.
+   * It provides various operations for manipulating colors, such as addition, subtraction,
+   * multiplication, division, and clamping.
+   */
   class Rgb {
   public:
+    /// @brief Default constructor initializes RGB components to zero (black).
     Rgb() = default;
+
+    /// @brief Constructor that initializes RGB components to specified values.
     SPECULA_CPU_GPU Rgb(Float r, Float g, Float b) : r(r), g(g), b(b) {}
 
+    /**
+     * @brief Computes the average of the RGB components.
+     *
+     * @return The average value of the red, green, and blue components.
+     */
     SPECULA_CPU_GPU Float average() const { return (r + g + b) / 3.0f; }
 
     SPECULA_CPU_GPU Rgb &operator+=(Rgb s) {
@@ -22,11 +47,11 @@ namespace specula {
 
     SPECULA_CPU_GPU Rgb operator+(Rgb s) const { return {r + s.r, g + s.g, b + s.b}; }
 
-    SPECULA_CPU_GPU Rgb *operator-=(Rgb s) {
+    SPECULA_CPU_GPU Rgb &operator-=(Rgb s) {
       r -= s.r;
       g -= s.g;
       b -= s.b;
-      return this;
+      return *this;
     }
 
     SPECULA_CPU_GPU Rgb operator-(Rgb s) const { return {r - s.r, g - s.g, b - s.b}; }
@@ -115,17 +140,49 @@ namespace specula {
     Float r = 0.0, g = 0.0, b = 0.0;
   };
 
+  /**
+   * @brief Computes the maximum of two RGB colors component-wise.
+   *
+   * @param a The first RGB color.
+   * @param b The second RGB color.
+   * @return The RGB color with each component being the maximum of the corresponding components of
+   * `a` and `b`.
+   */
   SPECULA_CPU_GPU inline Rgb max(Rgb a, Rgb b) {
     return {std::max(a.r, b.r), std::max(a.g, b.g), std::max(a.b, b.b)};
   }
 
+  /**
+   * @brief Linear interpolation between two RGB colors.
+   *
+   * @param t The interpolation factor, where 0 corresponds to `t0` and 1 corresponds to `t1`.
+   * @param t0 The start RGB color.
+   * @param t1 The end RGB color.
+   * @return A new RGB color that is the result of linear interpolation between `t0` and `t1`.
+   */
   SPECULA_CPU_GPU inline Rgb lerp(Float t, Rgb t0, Rgb t1) { return (1 - t) * t0 + t * t1; }
 
+  /**
+   * @brief Clamps the RGB color components to a specified range.
+   *
+   * @tparam U Value type for the minimum bound.
+   * @tparam V Value type for the maximum bound.
+   * @param rgb The RGB color to clamp.
+   * @param min The minimum value for each component.
+   * @param max The maximum value for each component.
+   * @return A new RGB color with each component clamped to the specified range.
+   */
   template <typename U, typename V> SPECULA_CPU_GPU inline Rgb clamp(Rgb rgb, U min, V max) {
     return Rgb(clamp(rgb.r, min, max), clamp(rgb.g, min, max), clamp(rgb.b, min, max));
   }
 
-  template <typename U, typename V> SPECULA_CPU_GPU inline Rgb clamp_zero(Rgb rgb) {
+  /**
+   * @brief Clamps the RGB color components to a minimum of zero.
+   *
+   * @param rgb The RGB color to clamp.
+   * @return A new RGB color with each component clamped to a minimum of zero.
+   */
+  SPECULA_CPU_GPU inline Rgb clamp_zero(Rgb rgb) {
     return Rgb(std::max<Float>(0, rgb.r), std::max<Float>(0, rgb.g), std::max<Float>(0, rgb.b));
   }
 } // namespace specula
--- include/specula/util/color/xyz.hpp
+++ include/specula/util/color/xyz.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file xyz.hpp
+ * @brief XYZ color class definition and operations
+ *
+ * This file defines the `Xyz` class, which represents a color in the CIE XYZ
+ * color space. It includes various operations such as addition, subtraction,
+ * multiplication, division, clamping, and interpolation.
+ */
+
 #ifndef INCLUDE_COLOR_XYZ_HPP_
 #define INCLUDE_COLOR_XYZ_HPP_
 
@@ -6,18 +15,54 @@
 #include "specula/util/vecmath/tuple2.hpp"
 
 namespace specula {
+
+  /**
+   * @class Xyz
+   * @brief A class representing a color in the CIE XYZ color space.
+   *
+   * The `Xyz` class encapsulates a color defined by its X, Y, and Z
+   * components. It provides various operations for manipulating colors, such
+   * as addition, subtraction, multiplication, division, and clamping.
+   *
+   * XYZ color space is a linear color space that serves as a device-independent
+   * model for color representation. It is based on human vision and is used as
+   * a reference for other color spaces. The components X, Y, and Z correspond
+   * to the human perception of color and are related to the biological response
+   * of the rods and cones in the human eye.
+   */
   class Xyz {
   public:
+    /// @brief Default constructor initializes XYZ components to zero.
     Xyz() = default;
+
+    /// @brief Constructor that initializes XYZ components to specified values.
     SPECULA_CPU_GPU Xyz(Float x, Float y, Float z) : x(x), y(y), z(z) {}
 
-    SPECULA_CPU_GPU static Xyz from_xyY(Point2f xy, Float y = 1) {
+    /**
+     * @brief Creates an `Xyz` color from chromaticity coordinates and luminance.
+     *
+     * @param xy The chromaticity coordinates (x, y).
+     * @param Y The luminance, defaults to 1.
+     * @return The corresponding `Xyz` color.
+     */
+    SPECULA_CPU_GPU static Xyz from_xyY(Point2f xy, Float Y = 1) {
       if (xy.y == 0)
         return Xyz(0.0, 0.0, 0.0);
-      return Xyz(xy.x * y / xy.y, y, (1 - xy.x - xy.y) * y / xy.y);
+      return Xyz(xy.x * Y / xy.y, Y, (1 - xy.x - xy.y) * Y / xy.y);
     }
 
+    /**
+     * @brief Computes the average of the XYZ components.
+     *
+     * @return The average value of the X, Y, and Z components.
+     */
     SPECULA_CPU_GPU Float average() const { return (x + y + z) / 3.0f; }
+
+    /**
+     * @brief Computes the chromaticity coordinates (x, y) from the XYZ color.
+     *
+     * @return A `Point2f` containing the (x, y) chromaticity coordinates.
+     */
     SPECULA_CPU_GPU Point2f xy() const { return Point2f(x / (x + y + z), y / (x + y + z)); }
 
     SPECULA_CPU_GPU Xyz &operator+=(Xyz s) {
@@ -26,6 +71,7 @@ namespace specula {
       z += s.z;
       return *this;
     }
+
     SPECULA_CPU_GPU Xyz operator+(Xyz s) const { return {x + s.x, y + s.y, z + s.z}; }
 
     SPECULA_CPU_GPU Xyz &operator-=(Xyz s) {
@@ -47,6 +93,7 @@ namespace specula {
       z *= s.z;
       return *this;
     }
+
     SPECULA_CPU_GPU Xyz operator*(Xyz s) const { return {x * s.x, y * s.y, z * s.z}; }
 
     SPECULA_CPU_GPU Xyz &operator*=(Float a) {
@@ -130,14 +177,38 @@ namespace specula {
     Float x = 0.0f, y = 0.0f, z = 0.0f;
   };
 
+  /**
+   * @brief Clamps the XYZ color components to a specified range.
+   *
+   * @tparam U Value type for the minimum bound.
+   * @tparam V Value type for the maximum bound.
+   * @param xyz The XYZ color to clamp.
+   * @param min The minimum value for each component.
+   * @param max The maximum value for each component.
+   * @return A new XYZ color with each component clamped to the specified range.
+   */
   template <typename U, typename V> SPECULA_CPU_GPU inline Xyz clamp(Xyz xyz, U min, V max) {
     return Xyz(clamp(xyz.x, min, max), clamp(xyz.y, min, max), clamp(xyz.z, min, max));
   }
 
+  /**
+   * @brief Clamps the XYZ color components to a minimum of zero.
+   *
+   * @param xyz The XYZ color to clamp.
+   * @return A new XYZ color with each component clamped to a minimum of zero.
+   */
   template <typename U, typename V> SPECULA_CPU_GPU inline Xyz clamp_zero(Xyz xyz) {
     return Xyz(std::max<Float>(0, xyz.x), std::max<Float>(0, xyz.y), std::max<Float>(0, xyz.z));
   }
 
+  /**
+   * @brief Linear interpolation between two XYZ colors.
+   *
+   * @param t The interpolation factor, where 0 corresponds to `t0` and 1 corresponds to `t1`.
+   * @param t0 The start XYZ color.
+   * @param t1 The end XYZ color.
+   * @return A new XYZ color that is the result of linear interpolation between `t0` and `t1`.
+   */
   SPECULA_CPU_GPU inline Xyz lerp(Float t, Xyz t0, Xyz t1) { return (1 - t) * t0 + t * t1; }
 } // namespace specula
 
