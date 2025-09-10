33d627c571cd3aa301a21ed6441b39c945df3a50
Arden Rasmussen <ardenisthebest@gmail.com>
2025-09-10 11:43:39 -0700

    chore(spectrum): imported more builtin spectrums

--- src/util/spectrum/functions.cpp
+++ src/util/spectrum/functions.cpp
@@ -1,5 +1,7 @@
 #include "util/spectrum/functions.hpp"
 
+#include "util/color/xyz.hpp"
+#include "util/spectrum/constants.hpp"
 #include "util/spectrum/rgb_illuminant_spectrum.hpp"
 #include "util/spectrum/spectra.hpp"
 #include "util/spectrum/spectrum.hpp"
@@ -9,4 +11,10 @@ specula::Float specula::spectrum_to_photometric(Spectrum s) {
     s = s.cast<RgbIlluminantSpectrum>()->illuminant();
   }
   return inner_product(&spectra::Y(), s);
+}
+
+specula::Xyz specula::spectrum_to_xyz(Spectrum s) {
+  return Xyz(inner_product(&spectra::X(), s), inner_product(&spectra::Y(), s),
+             inner_product(&spectra::Z(), s)) /
+         CIE_Y_INTEGRAL;
 }
